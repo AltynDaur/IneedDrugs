61a7d5103cef0473b4ece8159fba10dbbb0f8920
AltynDaur <altyndaur@gmail.com>
2015-04-27 23:06:49 +0600

    new select box impl

--- webapp/startPage/searchByName/searchByName.controller.js
+++ webapp/startPage/searchByName/searchByName.controller.js
@@ -1,19 +1,13 @@
 (function(){
     angular.module('searchByName').controller('SearchByNameController', SearchByNameController);
-    var closed, opened;
-    opened = 'opened';
-    closed = 'closed';
+
     function SearchByNameController($scope,$http, $filter, $q){
 
-        $scope.selectedMedicine = null;
+        $scope.selected = null;
         $scope.getAllMedicines = function(){
             $http.get("/IneedDrugs/resources/js.json").success(function(data){
                 $scope.medicines = data.nutritionalcare;
             });
-        };
-
-        $scope.selectionChanged = function(medicine){
-            $scope.medicineInfo = medicine;
         };
 
         $scope.analogs = [
@@ -48,40 +42,16 @@
                 }
             }
         ];
-        $scope.selected = '';
-        $scope.created = false;
-        $scope.state = closed;
-        $scope.change = function() {
-            if($scope.query.name.length < 3){
-                return $scope.state = 'closed';
+        $scope.selectionChanged = function(value) {
+            if(value.length < 3){
+                $scope.medicines = [];
             } else {
                 $scope.getAllMedicines();
-                var filtered;
-                filtered = $filter('filter')($scope.medicines, $scope.query.name);
-                return $scope.state = filtered.length > 0 ? opened : 'closed';
             }
 
         };
-        $scope.select = function(item) {
-            $scope.selected = item;
-            return $scope.state = closed;
-        };
-        return $scope.clear = function() {
-            $scope.query = '';
-            $scope.selected = '';
-            return $scope.cleared = true;
-        };
 
     };
-    angular.module('searchByName').directive('xngFocus', xngFocusDirective);
-    function xngFocusDirective(){
 
-        return function(scope, element, attrs) {
-            return scope.$watch(attrs.xngFocus, function(newValue) {
-                console.log(newValue);
-                return newValue && element[0].focus();
-            });
-        };
-    };
 })();
 
--- webapp/startPage/searchByName/searchByName.module.js
+++ webapp/startPage/searchByName/searchByName.module.js
@@ -1,3 +1,3 @@
 (function(){
-    angular.module('searchByName',[]);
+    angular.module('searchByName',['acute.select']);
 })();
