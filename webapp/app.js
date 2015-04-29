cd945e3b4ecf1ade3bff1152dbe03bb56ef0360f
Dauren_Altynbekov <altyndaur@gmail.com>
2015-04-29 10:16:45 +0600

    minor changes

--- webapp/app.js
+++ webapp/app.js
@@ -13,11 +13,25 @@
 
         $translateProvider.translations('ru',{
             CHANGELANGUAGE:'Сменить язык',
-            PROPOSEMEDICINE: 'Предложить добавить лекарство'
+            PROPOSEMEDICINE: 'Предложить добавить лекарство',
+            ENTERNAME: 'Пожалуйста введите название лекарства (как минимум 3 символа)',
+            ENTERACTIVE:'Пожалуйста введите активный элемент лекарства (как минимум 2 символа)',
+            MEDICINE:'Лекарство',
+            MEDICINECOST: 'Стоимость лекарства',
+            MEDICINEDESCR: 'Описание лекарства',
+            ACTIVEELEMENT: 'Активный элемент',
+            DRUGSTORE:'Аптека'
         });
         $translateProvider.translations('en',{
             CHANGELANGUAGE: 'Change language',
-            PROPOSEMEDICINE: 'Propose add drug'
+            PROPOSEMEDICINE: 'Propose add drug',
+            ENTERNAME: 'Please enter name of medicine(at least 3 symbols)',
+            ENTERACTIVE:'Please enter name of active element of medicine(at least 2 symbols)',
+            MEDICINE:'Medicine',
+            MEDICINECOST: 'Medicine cost',
+            MEDICINEDESCR: 'Description',
+            ACTIVEELEMENT: 'Active element',
+            DRUGSTORE:'Drugstore'
         });
         $translateProvider.preferredLanguage('en');
     };
